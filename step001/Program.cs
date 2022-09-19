087ba848bbc1a14fa62cc3461ec341c1e5b96ba2
Aivenest <aivenest@gmail.com>
2022-09-19 23:19:46 +0500

    step001

--- step001/Program.cs
+++ step001/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* void Insert(int a)
+{
+    Console.WriteLine("Vstavit`" + a);
+}
+
+int CreateNumber()
+{
+
+    return 1;
+}*/
+
+int CreateNumber()
+{
+    int number = new Random().Next(10, 100);
+    return number;
+}
+
+int MaxDigit(int number)
+{
+    int Decimal = number / 10;
+    int Ed = number % 10;
+
+    if (Decimal >= Ed)
+    {
+        return Decimal;
+    }
+    else
+    {
+        return Ed;
+    }
+ 
+}
+
+/*int a = CreateNumber();
+Console.WriteLine(a);
+int maxDigit = MaxDigit(a);
+Console.WriteLine(maxDigit);
+*/
+
+Console.WriteLine(CreateNumber());
+Console.WriteLine(MaxDigit(CreateNumber()));

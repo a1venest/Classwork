3770cdfad357096073d350b2653e834f4a25a776
Aivenest <aivenest@gmail.com>
2022-09-21 22:01:33 +0500

    step003 compleated

---
+++ step003/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Vvedite chislo 1.");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Vvedite chislo 2.");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+void Div(int number1, int number2)
+{
+    if (number2 % number1 == 0)
+    {
+        Console.WriteLine("Kratno");
+    }
+    else
+    {
+        Console.WriteLine("Ne kratno, ostatok: " + (Convert.ToDouble(number2 % number1)) + ".");
+    }
+}
+
+Div(number1, number2);
